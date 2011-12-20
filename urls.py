6dac947c4b5bf798535c2b010e1a86f06fb3467e
Rostislav <cherost@gmail.com>
2011-12-20 23:01:52 +0200

    At home

--- urls.py
+++ urls.py
@@ -2,12 +2,15 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
+from production.views import *
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('cms.urls')),
+
+    (r'^tinymce/',include('tinymce.urls')),
 )
 
 if settings.DEBUG:
