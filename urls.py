55d97a497a1db010c0bc779bfec3e405dc83cac8
Rostislav <cherost@gmail.com>
2011-12-22 02:56:42 +0200

    at home

--- urls.py
+++ urls.py
@@ -2,15 +2,18 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
-from production.views import *
+from django.conf.urls.defaults import *
+#from production.views import *
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^products/$', 'production.views.ProductionView', name='products'),
     url(r'^', include('cms.urls')),
 
     (r'^tinymce/',include('tinymce.urls')),
+
 )
 
 if settings.DEBUG:
